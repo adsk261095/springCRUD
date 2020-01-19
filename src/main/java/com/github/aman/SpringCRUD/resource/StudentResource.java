c9ecb3909cab219e0f38c6dd15c616672e0101b0
adsk261095 <adsk261095@gmail.com>
2020-01-19 22:48:55 +0530

    adding post api

--- src/main/java/com/github/aman/SpringCRUD/resource/StudentResource.java
+++ src/main/java/com/github/aman/SpringCRUD/resource/StudentResource.java
@@ -3,11 +3,9 @@ package com.github.aman.SpringCRUD.resource;
 import com.github.aman.SpringCRUD.model.Student;
 import com.github.aman.SpringCRUD.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class StudentResource {
@@ -15,17 +13,19 @@ public class StudentResource {
     @Autowired
     private StudentRepository studentRepository;
 
-    @RequestMapping("/getAllStudent")
+    @GetMapping("/getAllStudent")
     public Iterable<Student> getAllStudents(){
         return studentRepository.findAll();
+
     }
-    @RequestMapping("/getStudent")
-    public Student getStudent(@RequestParam(value="id", defaultValue = "0") String id, @RequestParam(value="name", defaultValue = "Aman") String name){
-        try{
-            return new Student(Integer.parseInt(id), name);
-        }
-        catch(Exception e){
-            return new Student(0, null);
-        }
+    @GetMapping("/getStudent")
+    public Optional<Student> getStudent(@RequestParam(value="id", defaultValue = "0") String id){
+        return studentRepository.findById(Integer.parseInt(id));
+    }
+
+    @PostMapping("/addStudent")
+    public Iterable<Student> addStudent(@RequestBody Student student){
+        studentRepository.save(student);
+        return studentRepository.findAll();
     }
 }
