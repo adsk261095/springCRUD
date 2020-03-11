6f08d1acfda658ac8d6aa846eb2222035339d29a
adsk261095 <adsk261095@gmail.com>
2020-03-11 21:00:05 +0530

    adding caching facility using redis

--- src/main/java/com/github/aman/SpringCRUD/model/Student.java
+++ src/main/java/com/github/aman/SpringCRUD/model/Student.java
@@ -1,16 +1,17 @@
 package com.github.aman.SpringCRUD.model;
 
 
-import javax.persistence.Id;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
+import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
-public class Student {
+public class Student  implements Serializable {
+    private static final long serialVersionUID = 7156526077883281623L;
+
     @Id
-    @GeneratedValue
     @Column(name="ID")
+    @SequenceGenerator(name = "SEQ_GEN", sequenceName = "SEQ_USER", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_GEN")
     private int id;
 
     @Column(name="Name")
--- src/main/java/com/github/aman/SpringCRUD/resource/StudentResource.java
+++ src/main/java/com/github/aman/SpringCRUD/resource/StudentResource.java
@@ -2,9 +2,13 @@ package com.github.aman.SpringCRUD.resource;
 
 import com.github.aman.SpringCRUD.model.Student;
 import com.github.aman.SpringCRUD.repository.StudentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Optional;
 
 @RestController
@@ -13,25 +17,42 @@ public class StudentResource {
     @Autowired
     private StudentRepository studentRepository;
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     @GetMapping("/getAllStudent")
     public Iterable<Student> getAllStudents(){
         return studentRepository.findAll();
-
     }
+
+    //get request
+    @Cacheable(value = "student", key="#id")
     @GetMapping("/getStudent")
     public Optional<Student> getStudent(@RequestParam(value="id", defaultValue = "0") String id){
+        logger.info("Getting student with id: " + id);
         return studentRepository.findById(Integer.parseInt(id));
     }
 
+    //get request
+    @Cacheable(value = "student", key="#result.id")
     @GetMapping("/getStudentByName")
     public Iterable<Student> findStudentByName(@RequestParam(value="name", defaultValue = "Aman") String name){
-//        @RequestParam(value="name", defaultValue = "") String name
         return studentRepository.findStudentByName(name);
     }
 
+    //add request
+    @CachePut(value="student", key="#result.id")
     @PostMapping("/addStudent")
-    public Iterable<Student> addStudent(@RequestBody Student student){
+    public Student addStudent(@RequestBody Student student){
+        logger.info("Adding student with id: " + student.getId());
         studentRepository.save(student);
-        return studentRepository.findAll();
+        return student;
+    }
+
+    //delete request
+    @CacheEvict(value = "student", key="#id")
+    @DeleteMapping("/deleteStudentById")
+    public void deleteStudentByID(@RequestParam(value="id") int id) {
+        logger.info("deleting student with id {}", id);
+        studentRepository.deleteById(id);
     }
 }
